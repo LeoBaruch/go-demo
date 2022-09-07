e7200a9fa44918ff53e0075dc2c3c697ad18ad02
gexiaolong <1013363134@qq.com>
2022-09-07 23:32:54 +0800

    feat: 查询在线用户

--- im/user.go
+++ im/user.go
@@ -46,7 +46,23 @@ func (this *User) Offline() {
 	this.server.BroadCast(this, "下线")
 }
 
+func (this *User) SendMsg(msg string) {
+	this.conn.Write([]byte(msg + "\n"))
+}
+
 func (this *User) DoMessage(msg string) {
+	// 当msg为who时, 查询当前所有在线用户
+	if msg == "who" {
+		this.server.mapLock.Lock()
+		for _, user := range this.server.OnlineMap {
+			onlineMsg := "[" + user.Addr + "]" + user.Name + ":" + "在线...\n"
+
+			this.SendMsg(onlineMsg)
+		}
+		this.server.mapLock.Unlock()
+
+		return
+	}
 	this.server.BroadCast(this, msg)
 }
 
