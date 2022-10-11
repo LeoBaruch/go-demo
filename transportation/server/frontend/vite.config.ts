3168eb54b42b2051a48f24818112e33f8fc45474
iyb-gexiaolong001 <gexiaolong@iyunbao.com>
2022-10-11 12:55:30 +0800

    feat: frontend 调试局域网启动

--- transportation/server/frontend/vite.config.ts
+++ transportation/server/frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:27149/',
